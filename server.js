525981946e9a6d32529e64aa9c2f237d384ad6d1
shkodard <shkodard@students.zhaw.ch>
2024-05-25 03:12:48 +0200

    update2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Es ist 03:10 und ich verzweifle an DevOps..');
+  res.send('Es ist 03:12 und ich verzweifle an DevOps..');
 });
 
 app.listen(PORT, HOST);
