df98e76290589113303814d48ff6bd7ced21e384
shkodard <shkodard@students.zhaw.ch>
2024-05-25 03:10:55 +0200

    update1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 :-)))');
+  res.send('Es ist 03:10 und ich verzweifle an DevOps..');
 });
 
 app.listen(PORT, HOST);
