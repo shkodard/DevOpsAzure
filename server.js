576f8da4443754286de32d44e64d4f582ac13174
shkodard <shkodard@students.zhaw.ch>
2024-05-25 11:39:25 +0200

    update-hab-aufgehört-zu-zählen

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Es ist 03:19 und ich verzweifle an DevOps.. wieso läufts nichts?');
+  res.send('Es ist 11:38 und ich verzweifle an DevOps.. neuer Tag, neues Glück?');
 });
 
 app.listen(PORT, HOST);
